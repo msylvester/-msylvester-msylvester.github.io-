a886ad8bd2acf5e6844c32d60c914e8a106ee23a
Mike Ess <msylvest55@gmail.com>
2022-03-26 05:25:49 -0700

    update bro

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
